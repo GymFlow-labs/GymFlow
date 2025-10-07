20f8d2190dcc4ee2bdcc640bfc06c17121aaf2f8
Artem <art.kriukov@gmail.com>
2025-10-07 16:42:43 +0300

    refactor: rename recorHistoryVC to recordHistoryView

---
+++ GymFlow/Features/RecordHistory/RecordHistoryView.swift
@@ -0,0 +1,15 @@
+//
+//  RecordHistoryView.swift
+//  GymFlow
+//
+//  Created by Artem Kriukov on 05.10.2025.
+//
+
+import SwiftUI
+
+struct RecordHistoryView: View {
+    var body: some View {
+        Text("Экран выбора упражнения")
+            .navigationTitle("Упражнение")
+    }
+}
