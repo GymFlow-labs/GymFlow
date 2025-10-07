8697f016c0d1b86eb2fd5dd76f0e83c379cf9297
Artem <art.kriukov@gmail.com>
2025-10-07 16:52:55 +0300

    feat: add destination to RecordHistoryView

--- GymFlow/Features/AddRecord/AddRecordView.swift
+++ GymFlow/Features/AddRecord/AddRecordView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct AddRecordView: View {
     var body: some View {
-        VStack {
-            RowButtonView(text: "упражнение", textIcon: .barbell) {
-                print("RowButtonView Action упражнение")
+        NavigationStack {
+            VStack {
+                NavigationLink(destination: RecordHistoryView()) {
+                    RowButtonView(text: "упражнение", textIcon: .barbell)
+                }
+                
+                .padding()
+                
+                RowButtonView(text: "Календарь", textIcon: .calendar)
+                .padding()
             }
-            .padding()
-            
-            RowButtonView(text: "Календарь", textIcon: .calendar) {
-                print("RowButtonView Action Календарь")
-            }
-            .padding()
         }
     }
 }
@@ -30,34 +31,29 @@ struct AddRecordView: View {
 struct RowButtonView: View {
     private let text: String
     private let textIcon: Image
-    private var action: () -> Void?
 
-    init(text: String, textIcon: Image, action: @escaping () -> Void?) {
+    init(text: String, textIcon: Image) {
         self.text = text
         self.textIcon = textIcon
-        self.action = action
     }
-
+    
     var body: some View {
-        Button {
-            action()
-        } label: {
-            HStack {
-                textIcon
-                    .resizable()
-                    .frame(width: 30, height: 30)
-
-                Text(text)
-                    .font(.title3)
-                Spacer()
-                Image.chevron
-                    .resizable()
-                    .frame(width: 20, height: 20)
-            }
-            .foregroundStyle(Color.primaryTextColor)
-            .padding()
+        HStack {
+            textIcon
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Text(text)
+                .font(.title3)
+            Spacer()
+            Image.chevron
+                .resizable()
+                .frame(width: 20, height: 20)
         }
+        .foregroundStyle(Color.primaryTextColor)
+        .padding()
         .background(Color.cellBackgroundColor)
         .cornerRadius(12)
     }
+        
 }
